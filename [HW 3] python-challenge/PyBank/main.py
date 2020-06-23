a7bcaaa181340a446799b87ffce3ad8ef0806e3d
James Nguyen <jamesnguyen0@gmail.com>
2020-06-22 21:53:08 -0700

    changed avgChange calculation

--- [HW 3] python-challenge/PyBank/main.py
+++ [HW 3] python-challenge/PyBank/main.py
@@ -6,9 +6,13 @@ import csv
 months = 0
 netChange = 0
 avgChange = 0
-totalAbsoluteChanges = 0
 maxIncrease = ["", 0]
 maxDecrease = ["", 0]
+
+#change calculations
+current = 0
+previous = 0
+changeInChange = 0
 
 #read files
 pybank_csv = os.path.join("Resources","budget_data.csv")
@@ -16,23 +20,32 @@ with open(pybank_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
 
     #skip header   
-    next(csvreader)
+    header = next(csvreader)
     
     #loop through each row
     for row in csvreader:
-        #calculations
+        #basic calculations
         months += 1
-        netChange += int(row[1])
-        totalAbsoluteChanges += abs(int(row[1]))
+        netChange += int(row[1]) 
+        current = int(row[1])
+        
+        #don't execute if current is first value in list
+        if not(previous == 0):
+            changeInChange = current - previous
+            #sum the change in changes
+            avgChange += changeInChange
 
-        if int(row[1]) > int(maxIncrease[1]):
+        if changeInChange > int(maxIncrease[1]):
             maxIncrease[0] = row[0]
-            maxIncrease[1] = row[1]
-        if int(row[1]) < int(maxDecrease[1]):
+            maxIncrease[1] = changeInChange
+        if changeInChange < int(maxDecrease[1]):
             maxDecrease[0] = row[0]
-            maxDecrease[1] = row[1]
+            maxDecrease[1] = changeInChange
     
-    avgChange = round(totalAbsoluteChanges/months, 2)
+        previous = int(row[1])    
+    
+    #calculate true average
+    avgChange = round(avgChange/(months - 1), 2)
 
     #output to console
     print("Financial Analysis")
@@ -51,8 +64,8 @@ with open(pybank_csv) as csvfile:
     text.append("Total Months: " + str(months))
     text.append("Total: $" + str(netChange))
     text.append("Average Change: $" + str(avgChange))
-    text.append("Greatest Increase in Profits: " + maxIncrease[0] + " $" + str(maxIncrease[1]))
-    text.append("Greatest Decrease in Profits: " + maxDecrease[0] + " $" + str(maxDecrease[1]))
+    text.append("Greatest Increase in Profits: " + maxIncrease[0] + " $(" + str(maxIncrease[1]) + ")")
+    text.append("Greatest Decrease in Profits: " + maxDecrease[0] + " $(" + str(maxDecrease[1]) + ")")
     
     outputtext = zip(text)
 
